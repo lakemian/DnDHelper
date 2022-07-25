75b529c1c8d695ef2f754105c7856f4a92c3511c
lakemian <92057254+lakemian@users.noreply.github.com>
2022-07-25 10:45:56 -0400

    Files for project
    dummyfile (sorry for strange names) contains info about gemstones and art objects such as price, properties, description, ect. This info gets turned into objects in item.py
    item.py parses in all the files and creates objects with their data. It also contains search methods.
    magicitem.csv contains info about the contents of magic item drops. I havent gotten this part to work yet, and idk if we wanna include this part. If we have extra time maybe.
    main.py contains the search and sort functions and a super basic "ui". the ui could be improved upon for sure.
    realMagicItem.csv is the same as magic item, not sure if it works at this time.

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
-import csv
+from item import *
 
 
-class Student(object):
-    def __init__(self, name, year, gpa):
-        self.name = name
-        self.year = year
-        self.gpa = gpa
+def search():
+    flag = 0
+    x = input("Item name: ")
+    for i in range(len(item_list)):
+        if item_list[i].get_name().lower() == x.lower():
+            item_list[i].get_stats()
+            flag = 1
+    if flag == 0:
+        print("Item not found")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def search_magic():
+    flag = 0
+    x = input("Item name: ")
+    for i in range(len(magic_item_list)):
+        if magic_item_list[i].get_name().lower() == x.lower():
+            magic_item_list[i].get_stats()
+            flag = 1
+    if flag == 0:
+        print("Item not found")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def list_items():
+    y = input("Choose type to list:\n1. Items\n4. Magic Item\n").lower()
+    if y == "Items".lower() or y == "1":
+        for i in range(len(item_list)):
+            if isinstance(item_list[i], Item):
+                print(item_list[i].name)
+    elif y == "Weapons".lower() or y == "2":
+        for i in range(len(item_list)):
+            if isinstance(item_list[i], Weapon):
+                print(item_list[i].name)
+    elif y == "Armor".lower() or y == "3":
+        for i in range(len(item_list)):
+            if isinstance(item_list[i], Armor):
+                print(item_list[i].name)
+    elif y == "Magic Item".lower() or y == "4":
+        for i in range(len(magic_item_list)):
+            if isinstance(magic_item_list[i], MagicItem):
+                print(magic_item_list[i].name)
+    else:
+        print("Invalid Input")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def options():
+    flag = 1
+    while flag != 0:
+        options_str = "1. Search for item\n2. List items\n3. Search Magic Items\n0. End\n"
+        x = input(options_str).lower()
+        if x == "1" or x == "Search for item".lower():
+            search()
+        elif x == "2" or x == "List items".lower():
+            list_items()
+        elif x == "3" or x == "Search Magic Items":
+            search_magic()
+        elif x == "0" or x == "End".lower():
+            flag = 0
+
+
+Item.read_file()
+LootTable.read_loot_table_file()
+MagicItem.read_file()
+
+options()
